5d4f19b9a9e4feda725d2c0c267ffec7d2b25247
AmirjonKarimov2007 <amirjonkarimoff@gmail.com>
2024-06-21 20:55:05 +0500

    deploy

--- git.py
+++ git.py
@@ -1,4 +1,7 @@
 import os
-os.system('git add .')
-os.system('git commit -m deploy')
-os.system('git push -u origin main')
+def git_push():
+    os.system('git add .')
+    os.system('git commit -m deploy')
+    os.system('git push -u origin main')
+git = git_push()
+print(git)
